bf3514cc84760687eb7f85ac79a4014220be57f1
“Andrew <andrewwoods88@gmail.com>
2018-08-30 19:41:33 -0400

    get data from crypto api and display in view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
+  
   def index
+    require 'net/http'
+    require 'json'
+    
+    # create url to call
+    @url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+    @uri = URI(@url)
+    # pass url to get response
+    @response = Net::HTTP.get(@uri)
+    # parse the response
+    @news = JSON.parse(@response)
+
   end
 
   def prices
