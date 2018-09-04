35f6576fee9b477ed9d791a8bdd1b22612ab0d6e
“Andrew <andrewwoods88@gmail.com>
2018-09-04 12:54:59 -0400

    Search API for crypto currency

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,5 +22,24 @@ class HomeController < ApplicationController
   end
 
   def prices
+    require 'net/http'
+    require 'json'
+
+  # if @symbol 
+    if params[:sym].present?
+      @symbol = params[:sym]
+      # api only accepts upcase search queries
+      @symbol = @symbol.upcase
+
+      #Price Data
+      @quote_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + @symbol + '&tsyms=USD'
+      @quote_uri = URI(@quote_url)
+      @quote_response = Net::HTTP.get(@quote_uri)
+      @quote = JSON.parse(@quote_response)
+    else
+      redirect_to root_path, notice: 'please enter a crypto currency to search'
+    end
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   get 'home/prices'
+  post "home/prices" => 'home/prices'
 
   root 'home#index'
 end
